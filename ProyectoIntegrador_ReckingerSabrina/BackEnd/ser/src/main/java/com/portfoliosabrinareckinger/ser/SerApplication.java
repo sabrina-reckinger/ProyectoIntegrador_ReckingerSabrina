d73a9bb05d774cefaa346579aa217945979f7214
Sabrina Reckinger <sabrina.reckinger@gmail.com>
2022-08-19 16:58:46 -0300

    BackEnd

---
+++ ProyectoIntegrador_ReckingerSabrina/BackEnd/ser/src/main/java/com/portfoliosabrinareckinger/ser/SerApplication.java
@@ -0,0 +1,13 @@
+package com.portfoliosabrinareckinger.ser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_ReckingerSabrina/BackEnd/ser/src/test/java/com/portfoliosabrinareckinger/ser/SerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliosabrinareckinger.ser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
